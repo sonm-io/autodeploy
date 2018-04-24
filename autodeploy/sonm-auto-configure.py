8ab351ac5c63b191de9cee2adeb96c8213d13dae
Aleksander Efimov <aleksander_efimov@sonm.io>
2018-04-24 13:50:34 +0300

    init commit

---
+++ autodeploy/sonm-auto-configure.py
@@ -0,0 +1,97 @@
+import io
+import os
+import shutil
+import sys
+import subprocess
+import ruamel.yaml
+import requests
+from ruamel.yaml.scalarstring import DoubleQuotedScalarString
+from pathlib import Path
+
+
+def run_command(command):
+    p = subprocess.Popen(command,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    return iter(p.stdout.readline, b'')
+
+
+def run_cli_login():
+    _result = run_command(['sonmcli', 'login'])
+    _result_list = list(_result)
+    return _result_list[2][13:]
+
+
+def modify_ks_yaml(_keystore, _password, file):
+    with open(file, 'r') as stream:
+        try:
+            _file = ruamel.yaml.round_trip_load(stream, preserve_quotes=True)
+            _file['ethereum']['key_store'] = DoubleQuotedScalarString(_keystore)
+            _file['ethereum']['pass_phrase'] = DoubleQuotedScalarString(_password)
+            with io.open(file, 'w+', encoding='utf8') as outfile:
+                ruamel.yaml.round_trip_dump(_file, outfile)
+        except ruamel.yaml.YAMLError as exc:
+            print(exc)
+
+
+def modify_worker_yaml(_eth_addr, ip):
+    with open('/etc/sonm/worker-default.yaml', 'r') as stream:
+        try:
+            _file = ruamel.yaml.round_trip_load(stream, preserve_quotes=True)
+            _file['hub']['eth_addr'] = DoubleQuotedScalarString(_eth_addr)
+            _file['public_ip_addrs'] = [DoubleQuotedScalarString(ip)]
+            with io.open('/etc/sonm/worker-default.yaml', 'w+', encoding='utf8') as outfile:
+                ruamel.yaml.round_trip_dump(_file, outfile)
+        except ruamel.yaml.YAMLError as exc:
+            print(exc)
+
+
+def modify_hub_yaml(_home, ip):
+    with open('/etc/sonm/hub-default.yaml', 'r') as stream:
+        try:
+            _file = ruamel.yaml.round_trip_load(stream, preserve_quotes=True)
+            _file['cluster']['announce_endpoint'] = DoubleQuotedScalarString(ip + ':15010')
+            _file['cluster']['store']['endpoint'] = DoubleQuotedScalarString(_home + '/.sonm/boltdb')
+            with io.open('/etc/sonm/hub-default.yaml', 'w+', encoding='utf8') as outfile:
+                ruamel.yaml.round_trip_dump(_file, outfile)
+        except ruamel.yaml.YAMLError as exc:
+            print(exc)
+
+
+def prepare_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(os.path.dirname(path))
+
+
+passwd = sys.argv[1]
+
+home = str(Path.home())
+
+keystore_ = home + '/sonm-keystore/'
+
+prepare_dir(keystore_)
+prepare_dir('/root/.sonm/')
+prepare_dir(home + '/.sonm/')
+
+modify_ks_yaml(keystore_, passwd, '/etc/sonm/cli.yaml')
+modify_ks_yaml(keystore_, passwd, '/etc/sonm/node-default.yaml')
+modify_ks_yaml(keystore_, passwd, '/etc/sonm/worker-default.yaml')
+modify_ks_yaml(keystore_, passwd, '/etc/sonm/hub-default.yaml')
+
+shutil.copy('/etc/sonm/cli.yaml', home + '/.sonm/cli.yaml')
+shutil.copy('/etc/sonm/cli.yaml', '/root/.sonm/cli.yaml')
+
+eth_addr = run_cli_login().decode('utf-8').rstrip()
+
+ip = sys.argv[2]
+
+response = requests.post('http://95.216.141.161:8000/register', json={'addr': eth_addr, 'ip': ip})
+
+if response.status_code != 200:
+    print("!!!!! Autorefill failed. Please refill account with ethereum manually !!!!!")
+
+modify_hub_yaml(home, ip)
+modify_worker_yaml(eth_addr, ip)
+print('Your ethereum address is ' + eth_addr)
+print('Please request test eth on Rinkeby blockchain: https://faucet.rinkeby.io/')
+print('Or you may wait while we will send it to your address')
---
+++ autodeploy/sonm-create-ask-plan.py
@@ -0,0 +1,48 @@
+import io
+import json
+import subprocess
+
+from ruamel.yaml import ruamel
+
+
+def execute_command(_command):
+    _command.append('--out=json')
+    _result = run_command(_command)
+    _result_list = list(_result)
+    return json.loads(_result_list[0].decode('utf-8'))
+
+
+def run_command(command):
+    p = subprocess.Popen(command,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    return iter(p.stdout.readline, b'')
+
+
+def create_slot_yaml(_cpu, _ram):
+    try:
+        _data = {"duration": "168h",
+                 "resources": {"cpu_cores": _cpu, "ram_bytes": _ram, "gpu_count": "NO_GPU", "storage": "256mb",
+                               "network": {"in": "1Mb", "out": "1Mb", "type": "INCOMING"},
+                               "properties": {"operation-a": 1}}}
+        with io.open('slot.yaml', 'w+', encoding='utf8') as outfile:
+            ruamel.yaml.round_trip_dump(_data, outfile)
+    except ruamel.yaml.YAMLError as exc:
+        print(exc)
+
+
+worker_list = execute_command(['sonmcli', 'hub', 'worker', 'list'])
+
+ask_plan_list = execute_command(['sonmcli', 'hub', 'ask-plan', 'list'])
+
+if 'slots' in ask_plan_list:
+    for slot in ask_plan_list['slots']:
+        execute_command(['sonmcli', 'hub', 'ask-plan', 'remove', slot])
+
+for worker_id in worker_list['info']:
+    status = execute_command(['sonmcli', 'hub', 'worker', 'status', worker_id])
+    cpu = len(status['capabilities']['cpu'])
+    ram = status['capabilities']['mem']['total']
+    create_slot_yaml(cpu, ram)
+    execute_command(
+        ['sonmcli', 'hub', 'ask-plan', 'create', '0.00001', 'slot.yaml', '0xF03e9f834CEdfCa66781643A04738794870AAd32'])
