0a0cf19898a920decbcdb017b64dbc5b9de717a6
Aleksander Efimov <aleksander_efimov@sonm.io>
2018-05-29 17:40:48 +0300

    updated script for iaas

--- sonm-auto-deploy.sh
+++ sonm-auto-deploy.sh
@@ -1,51 +1,105 @@
-#!/bin/bash
-apt-get install -y python3-pip unzip jq
+#!/usr/bin/env bash
 
-if ! [ -x "$(command -v docker)" ]; then
-  curl -s https://get.docker.com/ | bash
-fi
+# Exit script as soon as a command fails.
+set -o errexit
 
-curl -s https://packagecloud.io/install/repositories/SONM/core/script.deb.sh | bash
-
-apt-get install -y sonm-cli sonm-node sonm-hub sonm-worker
-
-export LC_ALL="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
-dpkg-reconfigure locales
-
-pip3 install ruamel.yaml requests
-
-wget https://docs.sonm.io/status/SONM_MVP_0.3.6_configs.zip
-rm -rf /etc/sonm/
+worker_config="worker_default.yaml"
+node_config="node_default.yaml"
+cli_config="cli.yaml"
 actual_user=$(logname)
-home_dir=$(eval echo "~$(logname)")
-KSDIR=${home_dir}/sonm-keystore
-if [  -d "$KSDIR"  ]; then
-   mv $KSDIR $KSDIR.$(date +%s)
-fi
-mkdir /etc/sonm/
-unzip -j SONM_MVP_0.3.6_configs.zip -d /etc/sonm/
+mkdir -p ~/.sonm/
 
-MYIP=$(curl -s ipv4.icanhazip.com)
+install_docker() {
+	if ! [ -x "$(command -v docker)" ]; then
+		curl -s https://get.docker.com/ | bash
+	fi
+}
 
-green=`tput setaf 2`
-red=`tput setaf 1`
-reset=`tput sgr0`
-echo "${green}==============================================="
-echo "${green}Please enter PASSWORD for new ethereum account:"
-echo "${green}===============================================${reset}"
-read passwd < /dev/tty
-curl -s https://raw.githubusercontent.com/sonm-io/autodeploy/master/sonm-auto-configure.py | python3 - $passwd $MYIP
+install_dependency() {
+    apt-get install -y jq curl wget
+}
 
-chown -R $actual_user:$actual_user $KSDIR
-systemctl restart sonm-node sonm-hub sonm-worker
+read_password() {
+    green=`tput setaf 2`
+    reset=`tput sgr0`
+    echo "${green}==============================================="
+    echo "${green}Please enter PASSWORD for new ethereum account:"
+    echo "${green}===============================================${reset}"
+    read PASSWORD < /dev/tty
+}
 
-ip_blocked=$(curl -sX POST http://isitblockedinrussia.com -H 'Content-Type: application/json' -d '{"host":"'$MYIP'"}' | jq -r '.ips[] | .blocked | length')
-if [ "$ip_blocked" != "0" ]; then
-    echo "${red}Your ip address is blocked in Russia"
-    echo "${red}====================================${reset}"
-    exit 1
-fi
-curl -s https://raw.githubusercontent.com/sonm-io/autodeploy/master/sonm-get-eth.py | python3 - $MYIP
+download_artifacts() {
+	curl -s https://packagecloud.io/install/repositories/SONM/core/script.deb.sh | bash
+    apt-get install -y sonm-cli sonm-node sonm-worker
+}
 
-echo "${green}SONM installation successfull.${reset}"
+download_templates() {
+    wget https://raw.githubusercontent.com/sonm-io/autodeploy/master/worker_template.yaml
+    wget https://raw.githubusercontent.com/sonm-io/autodeploy/master/node_template.yaml
+    wget https://raw.githubusercontent.com/sonm-io/autodeploy/master/cli_template.yaml
+    wget https://raw.githubusercontent.com/sonm-io/autodeploy/master/variables.txt
+}
+
+load_variables() {
+    source variables.txt
+    export $(cut -d= -f1 variables.txt)
+}
+
+var_value() {
+    eval echo \$$1
+}
+
+modify_config() {
+    template="${1}"
+
+    vars=$(grep -oE '\{\{[A-Za-z0-9_]+\}\}' "${template}" | sort | uniq | sed -e 's/^{{//' -e 's/}}$//')
+
+    replaces=""
+    vars=$(echo $vars | sort | uniq)
+    for var in $vars; do
+        value=$(var_value $var | sed -e "s;\&;\\\&;g" -e "s;\ ;\\\ ;g")
+        value=$(echo "$value" | sed 's/\//\\\//g');
+        replaces="-e 's|{{$var}}|${value}|g' $replaces"
+    done
+
+    escaped_template_path=$(echo $template | sed 's/ /\\ /g')
+    eval sed $replaces "$escaped_template_path" > $2
+}
+
+resolve_gpu() {
+    if [[ $(lsmod | grep amd) ]]; then
+        GPU_SETTINGS="gpus:\n\\ \\ \\ \\ radeon: {}"
+    elif [[ $(lsmod | grep nvidia) ]]; then
+        GPU_SETTINGS="gpus:\n\\ \\ \\ \\ nvidia: {}"
+    else
+        GPU_SETTINGS=""
+    fi
+}
+
+install_dependency
+install_docker
+download_artifacts
+download_templates
+load_variables
+read_password
+
+#cli
+modify_config "cli_template.yaml" $cli_config
+mv $cli_config ~/.sonm/$cli_config
+MASTER_ADDRESS=$(sonmcli login | head -n 1| cut -c14-)
+
+#node
+modify_config "node_template.yaml" $node_config
+mv $node_config /etc/sonm/$node_config
+
+#worker
+resolve_gpu
+modify_config "worker_template.yaml" $worker_config
+mv $worker_config /etc/sonm/$worker_config
+systemctl start sonm-worker
+
+keystore_file=$(ls $WORKER_KEY_PATH)
+WORKER_ADDRESS=$(cat $WORKER_KEY_PATH/$keystore_file | jq '.address')
+
+#confirm worker
+sonmcli master confirm $WORKER_ADDRESS
