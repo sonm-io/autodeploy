87e829d0a05c055d274dffce0e124f7adccf8274
Aleksander Efimov <aleksander_efimov@sonm.com>
2018-11-15 11:41:13 +0300

    update uninstall script

--- sonm-uninstall.sh
+++ sonm-uninstall.sh
@@ -6,8 +6,8 @@ fi
 if systemctl is-active sonm-node; then
     systemctl stop sonm-node && echo "sonm-node stopped";
 fi
-if systemctl is-active sonm-hub; then
-    systemctl stop sonm-hub && echo "sonm-hub stopped";
+if systemctl is-active sonm-optimus; then
+    systemctl stop sonm-optimus && echo "sonm-optimus stopped";
 fi
 if  [[ $(dpkg --get-selections | grep -v deinstall | grep sonm | awk '{print $1}') ]]; then
     dpkg -P $(dpkg --get-selections | grep -v deinstall | grep sonm | awk '{print $1}')
