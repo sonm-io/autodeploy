7fd91a90fc3a30142deaf6669cfdb59715d63287
avsigaev <avsigaev@gmail.com>
2019-09-13 21:42:24 +0300

    opt

---
+++ hive/optimus_price.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+CARDS_NUM=`nvidia-smi -L | grep UUID | wc -l`
+
+mv /etc/sonm/optimus-default.yaml /etc/sonm/optimus-backup.yaml
+
+echo "sed 's/min_price: 0.01/min_price: `bc -l <<< "scale=2; $CARDS_NUM*$1"`/g' /etc/sonm/optimus-backup.yaml > /etc/sonm/optimus-default.yaml" | bash
